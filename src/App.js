746118dfbaeb87a07d42db432ffd3a3ff7575f5e
GRapple <grpel2108@gmail.com>
2021-04-16 17:21:21 +0900

    component 반복 출력

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
+function GRapple({name, id}) {
+  return (
+    <div>
+      <h3>I'm {name}</h3>
+      {id}
+    </div>
+  )
+}
+
+const list = [
+  {
+    "name": "GRapple",
+    "id": "grpel2108"
+  },
+  {
+    "name": "test11",
+    "id": "qwer"
+  }
+]
+
 function App() {
   return (
-    <div> hello!!</div>
+    <div>
+      {list.map(e => {
+        return <GRapple name={e.name} id={e.id}/>
+      })}
+    </div>
   );
 }
 
